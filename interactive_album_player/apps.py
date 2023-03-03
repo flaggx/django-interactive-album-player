f26dcbef6bea9d705415e8d62d47e15f3c53fe03
Andrew Mathews <flaggx@gmail.com>
2023-03-02 17:48:49 -0800

    Initial commit

---
+++ interactive_album_player/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InteractiveAlbumPlayerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'interactive_album_player'
