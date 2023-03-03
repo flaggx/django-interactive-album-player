9339ca41035ab4c4b040b8dee309647f58cb3cdd
Andrew Mathews <flaggx@gmail.com>
2023-03-02 18:30:31 -0800

    Added first view for webapp

---
+++ interactive_album_player/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from . import views
+
+app_name = 'interactive_album_player'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    ]
